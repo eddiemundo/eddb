c6a76d1d837b733c9ff58b37c7d2625162c82da6
eddiemundo <document_done@hotmail.com>
2012-05-15 22:52:33 -0400

    Implements: Node: init, incoming_edges, outgoing_edges, edges Edge: init Graph: init
    with accompanying unit tests

--- src/graphtest.py
+++ src/graphtest.py
@@ -1,207 +1,115 @@
-from graph import Node, Edge, Graph, ElementList
+from graph import Node, Edge, Graph, GraphError, EdgeList
 import unittest
 
 class TestGraph(unittest.TestCase):
 	def setUp(self):
 		pass
 	
-	def test_Node_init(self):
-		n = Node(0)
-		self.assertEqual(n.id, 0)
-		self.assertEqual(n.properties, {})
-		n = Node(1)
-		self.assertEqual(n.id, 1)
-		self.assertEqual(n.properties, {})
-		n = Node(2, {'type': 'user', 'username': 'N7Shepard'})
-		self.assertEqual(n.id, 2)
-		self.assertEqual(n.properties, {'type': 'user', 'username': 'N7Shepard'})
-		self.assertEqual(n.type, 'user')
-		self.assertEqual(n.username, 'N7Shepard')
-		
-	def test_Node(self):
-		n = Node(0)
-		n.name = 'jack'
-		self.assertEqual(n.properties['name'], 'jack')
-		n.gender = 'sexy'
-		self.assertEqual(n.properties['gender'], 'sexy')
+	def tearDown(self):
+		Node._current_id = 0
+		Edge._current_id = 0
 	
-	def test_Node_edges(self):
-		jack = Node(0, {'age': 21}, name='jack')
-		jill = Node(1, {'age': 21}, name='jill')
-		jon = Node(2, {'age': 22}, name='jon')
-		e0 = Edge(0, jack, 'loves', jill, intensity=100, ferocity=100)
-		e1 = Edge(1, jack, 'loves', jon)
-		e2 = Edge(2, jon, 'loves', jack)
-		e3 = Edge(3, jill, 'bangs', jack)
-		e4 = Edge(4, jill, 'hates', jon)
-		eall_set = set((e0, e1, e2, e3))
-		eloves_set = set((e0,e1,e2))
-		self.assertTrue(eall_set == set(jack.edges()))
-		self.assertTrue(e4 not in jack.edges())
-		self.assertTrue(eloves_set == set(jack.edges('loves')))
-		self.assertTrue(set([e2])==set(jack.edges('loves', 'incoming')))
-		self.assertTrue(set([e0]) == set(jack.edges('loves', intensity=100)))
-		self.assertTrue(set([]) == set(jack.edges('loves', properties={'ferocity':99}, intensity=100)))
-	
-	def test_Edge_init(self):
-		jack = Node(0)
-		jill = Node(1)
-		e = Edge(0, jack, 'loves', jill)
-		self.assertEqual(e.id, 0)
-		self.assertEqual(e.label, 'loves')
-		self.assertEqual(e.start_node, jack)
-		self.assertEqual(e.end_node, jill)
-		self.assertEqual(e.properties, {})
-		e = Edge(0, jack, 'loves', jill, {'intensity': 100, 'blindness': 100})
-		self.assertEqual(e.intensity, 100)
-		self.assertEqual(e.blindness, 100)
-		
-	def test_Edge(self):
-		jack = Node(0)
-		jill = Node(1)
-		e = Edge(0, jack, 'loves', jill)
-		e.label = 'hates'
-		e.intensity = 99
-		self.assertEqual(e.label, 'hates')
-		self.assertTrue('label' not in e.properties)
-		self.assertTrue(e.properties['intensity'] == 99)
-		
-	def test_ElementList(self):
-		jack = Node(0, {'age': 21}, name='jack')
-		jill = Node(1, {'age': 21}, name='jill')
-		jon = Node(2, {'age': 22}, name='jon')
-		e0 = Edge(0, jack, 'loves', jill, intensity=100, ferocity=100)
-		e1 = Edge(1, jack, 'loves', jon)
-		e2 = Edge(2, jon, 'loves', jack)
-		e3 = Edge(3, jill, 'bangs', jack)
-		e4 = Edge(4, jill, 'hates', jon)
-		self.assertTrue(set([e0]) == set(jack.edges().filter_by_property({'ferocity':100},intensity=100)))
-		
-	def test_Graph_init(self):
+	def test_graph_init(self):
 		g = Graph()
-		
-	def test_Graph_add_node(self):
-		g = Graph()
-		g.add_node({'type': 'user', 'username': 'Shepard'}, emailaddress='shepard@normandy.com',summonername='N7Shepard')
-		self.assertTrue(g._nodes[0].type == 'user')
-		self.assertTrue(g._nodes[0].username == 'Shepard')
-		self.assertTrue(g._nodes[0].emailaddress == 'shepard@normandy.com')
-		self.assertTrue(g._nodes[0].summonername == 'N7Shepard')
-		self.assertTrue(g._nodes[0].id == 0)
-		self.assertTrue(len(g._nodes) == 1)
-		g.add_node()
-		self.assertTrue(len(g._nodes) == 2)
-		self.assertTrue(1 in g._nodes)
-		self.assertTrue(g.add_node(name='Geronimo').name == 'Geronimo')
-		jack = g.add_node(name='jack')
-		self.assertEqual(jack.name, 'jack')
-		
-	def test_Graph_add_edge(self):
-		g = Graph()
-		jack = g.add_node()
-		jill = g.add_node()
-		g.add_edge(jack, 'loves', jill)
-		e = g.add_edge(jack, 'kills', jill)
-		self.assertTrue(len(g._edges) == 2)
-		self.assertTrue(g._edges[2].label=='loves')
-		self.assertTrue(g._edges[3].label=='kills')
-		self.assertEqual(e, g._edges[3])
-	
-	def test_Graph_remove_edge(self):
-		g = Graph()
-		jack = g.add_node()
-		jill = g.add_node()
-		g.add_edge(jack, 'loves', jill)
-		g.remove_edge(2)
-		self.assertTrue(len(jack.edges()) == 0)
-		self.assertTrue(len(jill.edges()) == 0)
-		self.assertTrue(len(g._edges) == 0)
-	
-	def test_Graph_remove_node(self):
-		g = Graph()
-		jack = g.add_node()
-		jill = g.add_node()
-		g.add_edge(jack, 'loves', jill)
-		g.add_edge(jack, 'kills', jill)
-		g.remove_node(0)
-		self.assertTrue(len(g._nodes) == 1)
-		self.assertTrue(len(g._edges) == 0)
-		self.assertEqual(jill, g._nodes[1])
-	
-	def test_Graph_find_reachable_by(self):
-		g = Graph()
-		
-		a = g.add_node()
-		b = g.add_node()
-		c = g.add_node()
-		d = g.add_node()
-		e = g.add_node()
-		f = g.add_node()
-		
-		g.add_edge(a, 'contains', b)
-		g.add_edge(a, 'contains', c)
-		g.add_edge(a, 'contains', d)
-		g.add_edge(b, 'contains', e)
-		g.add_edge(c, 'contains', e)
-		g.add_edge(e, 'contains', f)
-		g.add_edge(f, 'contains', a)
-		
-		iterator = g.find_reachable_nodes_from(a)
-		
-		self.assertEqual([b,c,d,e,f], list(iterator))
-		
-		iterator = g.find_reachable_nodes_from(a, contains='outgoing')
-		self.assertEqual([b,c,d,e,f], list(iterator))
-		iterator = g.find_reachable_nodes_from(a, contains='incoming')
-		self.assertEqual([f,e,b,c], list(iterator))
-		
-	def test_Graph_node(self):
-		g = Graph()
-		a = g.add_node()
-		b = g.add_node()
-		self.assertEqual(g.node(), a)
-		self.assertEqual(g.node(name='jack'), None)
-		self.assertEqual(g.node(1), b)
-		
-	def test_Graph_nodes(self):
-		g = Graph()
-		self.assertEqual(g.nodes(), ElementList([]))
-		a = g.add_node(name='jack')
-		b = g.add_node()
-		self.assertEqual(g.nodes(), ElementList([a, b]))
-		self.assertEqual(g.nodes(name='jack'), ElementList([a]))
-	
-	def test_Graph_edge(self):
-		g = Graph()
-		jack = g.add_node()
-		jill = g.add_node()
-		loves = g.add_edge(jack, 'loves', jill)
-		kills = g.add_edge(jack, 'kills', jill)
-		self.assertEqual(g.edge(label='loves'), loves)
-		self.assertEqual(g.edge(label='deads'), None)
-		self.assertEqual(g.edge(2), loves)
-	
-	def test_Graph_edges(self):
-		g = Graph()
-		jack = g.add_node()
-		jill = g.add_node()
-		self.assertEqual(g.edges(), ElementList())
-		loves = g.add_edge(jack, 'loves', jill, intensity=100)
-		kills = g.add_edge(jack, 'kills', jill)
-		self.assertEqual(g.edges(), ElementList([loves, kills]))
-		self.assertEqual(g.edges('loves'), ElementList([loves]))
-		self.assertEqual(g.edges('dead'), ElementList())
-		self.assertEqual(g.edges(intensity=100), ElementList([loves]))
-		
-	def test_Node_adjacent_nodes(self):
-		g = Graph()
-		jack = g.add_node()
-		jill = g.add_node(name='jill')
-		g.add_edge(jack, 'loves', jill)
-		self.assertEqual(jack.adjacent_nodes(name="jill")[0], jill)
-		self.assertEqual(jack.adjacent_node(name="jill"), jill)
-		
+		self.assertEqual(g._nodes, {})
+		self.assertEqual(g._edges, {})
 
+	def test_node_init(self):
+		self.assertRaises(GraphError, Node, 1)
+		self.assertRaises(GraphError, Node, {})
+		self.assertRaises(GraphError, Node, object())
 		
+		g = Graph()
+		n1 = Node(g)
+		
+		n2 = Node(g, type='user', name='joneses')
+		self.assertEqual(n2._properties['type'], 'user')
+		self.assertEqual(n2._properties['name'], 'joneses')
+		self.assertEqual(n2._id, 1)
+		self.assertEqual(n1._graph._nodes, {0: n1, 1: n2})
+		self.assertEqual(n2._graph._nodes, {0: n1, 1: n2})
+		
+	def test_node_attributes(self):
+		n = Node(Graph(), type='user', name='joneses')
+		self.assertEqual(n.type, 'user')
+		self.assertEqual(n.name, 'joneses')
+		
+		n.type = 'replay'
+		n.name = 'feeders'
+		
+		self.assertEqual(n.type, 'replay')
+		self.assertEqual(n.name, 'feeders')
+		self.assertEqual(n._properties['type'], 'replay')
+		self.assertEqual(n._properties['name'], 'feeders')
+		
+		n.hello = 'jackoff'
+		self.assertEqual(n.hello, 'jackoff')
+		self.assertEqual(n._properties['hello'], 'jackoff')
+	
+	def test_edge_init(self):
+		n1 = Node(Graph())
+		n2 = Node(Graph())
+		
+		self.assertRaises(GraphError, Edge, 1, 'friend', n2)
+		self.assertRaises(GraphError, Edge, n1, 'friend', 2)
+		self.assertRaises(GraphError, Edge, n1, 'friend', n2)
+		n1._graph = 1
+		self.assertRaises(GraphError, Edge, n1, 'friend', n2)
+		n1._graph = n2._graph
+		e1 = Edge(n1, 'friend', n2, weight=3)
+		
+		self.assertEqual(e1.label, 'friend')
+		self.assertEqual(e1.weight, 3)
+		self.assertEqual(e1._properties['weight'], 3)
+		self.assertEqual(e1.head, n1)
+		self.assertEqual(e1.tail, n2)
+		self.assertEqual(e1._id, 0)
+		
+		e2 = Edge(n1, 'friend', n2, weight=3)
+		
+		self.assertEqual(e2._id, 1)
+		self.assertEqual(n1._incoming_edges, [])
+		self.assertEqual(n1._outgoing_edges, [e1, e2])
+		self.assertEqual(n2._incoming_edges, [e1, e2])
+		self.assertEqual(n2._outgoing_edges, [])
+		self.assertEqual(n1._graph._edges, {0: e1, 1: e2})
+		self.assertEqual(n2._graph._edges, {0: e1, 1: e2})
+	
+	def test_node_incoming_edges(self):
+		g = Graph()
+		n1 = Node(g)
+		n2 = Node(g)
+		e1 = Edge(n1, 'hates', n2, love='lost')
+		e2 = Edge(n1, 'knows', n2)
+		self.assertIsInstance(n2.incoming_edges(), EdgeList)
+		self.assertEqual(n2.incoming_edges(), EdgeList([e1, e2]))
+		self.assertEqual(n2.incoming_edges(love='lost'), EdgeList([e1]))
+		self.assertEqual(n2.incoming_edges(love='dead'), EdgeList())
+		self.assertEqual(n1.incoming_edges(), EdgeList())
+	
+	def test_node_outgoing_edges(self):
+		g = Graph()
+		n1 = Node(g)
+		n2 = Node(g)
+		e1 = Edge(n1, 'hates', n2, love='lost')
+		e2 = Edge(n1, 'knows', n2)
+		self.assertIsInstance(n1.outgoing_edges(), EdgeList)
+		self.assertEqual(n1.outgoing_edges(), EdgeList([e1, e2]))
+		self.assertEqual(n1.outgoing_edges(love='lost'), EdgeList([e1]))
+		self.assertEqual(n1.outgoing_edges(love='dead'), EdgeList())
+		self.assertEqual(n2.outgoing_edges(), EdgeList())
+	
+	def test_node_edges(self):
+		g = Graph()
+		n1 = Node(g)
+		n2 = Node(g)
+		e1 = Edge(n1, 'hates', n2, love='lost')
+		e2 = Edge(n2, 'knows', n1)
+		self.assertIsInstance(n1.edges(), EdgeList)
+		self.assertEqual(n1.edges(), EdgeList([e1, e2]))
+		self.assertEqual(n1.edges(love='lost'), EdgeList([e1]))
+		self.assertEqual(n1.edges(love='dead'), EdgeList())
+		self.assertEqual(n2.edges(), EdgeList([e2, e1]))
+	
 if __name__ == '__main__':
 	unittest.main()
